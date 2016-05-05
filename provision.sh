af4c4038c81fad45f356800d73a186a737f36aec
Justas Brazauskas <brazauskasjustas@gmail.com>
2016-05-04 22:25:22 -0400

    Run on port 80, add package.json

--- provision.sh
+++ provision.sh
@@ -5,6 +5,10 @@ nvm alias default 6
 npm install -g npm@2
 n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
 npm install -g pm2
-pm2 ping
 sudo mkdir -p /var/www
 sudo chown vagrant:vagrant -R /var/www
+sudo apt-get install authbind
+sudo touch /etc/authbind/byport/80
+sudo chown vagrant /etc/authbind/byport/80
+sudo chmod 755 /etc/authbind/byport/80
+authbind --deep pm2 update
